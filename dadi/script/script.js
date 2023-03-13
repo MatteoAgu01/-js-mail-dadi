43550a4a94434ee1800d144017e3d188d168bfd9
MatteoAgu01 <matteoaguiari012@gmail.com>
2023-03-14 00:47:32 +0100

    add dice

---
+++ dadi/script/script.js
@@ -0,0 +1,34 @@
+﻿/*Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+
+// Listener button
+document.querySelector("button[name='rollBtn']").addEventListener("click", (roll));
+
+let games = 1;
+
+function roll(){
+	// Generate the dice roll
+	let userDice = (Math.random() * 5 + 1).toFixed(0);
+	let cpuDice = (Math.random() * 5 + 1).toFixed(0);
+
+	console.clear();
+
+	console.log("Game n." + games);
+	console.log("");
+
+	console.log("You: " + userDice);
+	console.log("CPU: " + cpuDice);
+
+	if(userDice > cpuDice){
+		console.log("User wins");
+	}
+	else if(userDice == cpuDice){
+		console.log("Tie");
+	}
+	else{
+		console.log("CPU wins");
+	}
+
+	games++;
+}
--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-﻿const enterBtn = document.getElementById('enterBtn');
+﻿
+/*Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+const enterBtn = document.getElementById('enterBtn');*/
 
 // Listeners
 enterBtn.addEventListener('click', (login));
